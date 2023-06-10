826438fa5203685fa2df196ffe58f845d70c2118
kaitlynclements <123575277+kaitlynclements@users.noreply.github.com>
2023-06-09 21:40:51 -0500

    Add files via upload

---
+++ Lab 1/program1.py
@@ -0,0 +1,10 @@
+'''
+Author: Kaitlyn Clements
+KUID: 3072622
+Date: 01/24/2022
+Lab: lab1
+Last Modified: 01/24/2022
+Purpose: Print output "This is my first lab exercise!"
+'''
+
+print ("This is my first lab exercise!")
